108e3d808ea9b799cbbde47b1bfbdd4543b01f68
rpaul <rpaul93@wgu.edu>
2024-08-03 18:27:58 -0400

    adds function to geocode addresses

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import geopandas as gpd
-from shapely.geometry import Point, Polygon
+import googlemaps
+from shapely.geometry import Point
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
 
 
-def is_eligible(point_coordinates):
-
+def is_eligible(coordinates):
     eligiblity_map = gpd.read_file("shapefile/SFH_MFH_Ineligible.shp")
     eligiblity_map = eligiblity_map.to_crs("EPSG:4326")
-    coordinates = Point(point_coordinates)
+    coordinates_point = Point(coordinates)
 
-    eligiblity_map.contains(coordinates)
+    eligiblity_map.contains(coordinates_point)
 
     inside_polygon = eligiblity_map.geometry.apply(lambda polygon: polygon.contains(coordinates)).any()
 
     return inside_polygon
 
 
+def address_to_coordinates(address):
+    configure()
+    return 0
+
+
+
 if __name__ == '__main__':
-    orlando = (-81.324921, 28.516623)
-    wedgefield = (-81.073523, 28.479016)
-
-    print(is_eligible(orlando))
-    print(is_eligible(wedgefield))
-
+    print(address_to_coordinates(input("Enter address: ")))
